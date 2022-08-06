bdffe2b6040f671e530da104d816f0a6ab3dbbbe
MuhammadLuthfi2003 <muhammad.luthfi2003@mail.ugm.ac.id>
2022-08-06 19:10:54 +0700

    reformatted the file to be readable

--- src/components/interface.js
+++ src/components/interface.js
@@ -1,32 +1,58 @@
 import React from 'react';
 
-import {Canvas, useLoader} from 'react-three-fiber';
-import {OrbitControls} from '@react-three/drei';
+import {Canvas, useLoader, useFrame} from 'react-three-fiber';
+import {Sphere, PresentationControls} from '@react-three/drei';
 import {TextureLoader} from 'three/src/loaders/TextureLoader'; 
 
 //import texture
 import jupiterTexture from '../images/jupiter-texture.png';
+import earthTexture from '../images/earth_texture.jpg';
+import marsTexture from '../images/mars_texture.jpg';
+import neptuneTexture from '../images/neptune_texture.jpg';
 
-function Interface() {
-    const colorMap = useLoader(TextureLoader, jupiterTexture);
+const textureArray = [jupiterTexture, earthTexture, marsTexture, neptuneTexture];
+
+function Planets({texture}) {
+    const colorMap = useLoader(TextureLoader, texture);
         
+    return(         
+        <Canvas className='canvas'>
+
+            <ambientLight intensity={0.5}/>
+            <directionalLight intensity={0.5} position={[-2,4,2]}/>
+
+            <PresentationControls 
+            cursor={true}
+            speed={2}
+            polar={[-Infinity, Infinity]}
+            azimuth={[-Infinity, Infinity]}
+            >
+                <Sphere args={[2,32,32]} >
+                    <meshStandardMaterial map={colorMap} attach='material' />
+                </Sphere>
+            </PresentationControls>
+
+        </Canvas>
+    );
+}
+
+
+class Interface extends React.Component{
+    render(){
         return(
-            <Canvas className='canvas'>
-                <OrbitControls enableZoom={false} />
-
-                <ambientLight intensity={1}/>
-                <directionalLight intensity={0.5} position={[-2,5,2]}/>
-                
-                    <mesh rotation={[0,0,0]}>
-                        <sphereGeometry attach="geometry" args={[1,32]} />
-                        <meshStandardMaterial map={colorMap}/>
-
-                    </mesh>
-                
-            </Canvas>
+            
+            <div className='planets'>
+                {
+                    textureArray.map((texture, index) => {
+                        return(
+                            <Planets texture={texture} key={index}/>
+                        )
+                    })
+                }    
+            </div> 
         );
-
     }
+}
 
 
 export default Interface;
